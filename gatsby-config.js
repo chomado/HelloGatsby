2460c24a88c3f7d40e97fe5f7e8e8fa4baf8d55b
ちょまど | Madoka Chiyoda <madoka.madoka@microsoft.com>
2020-08-03 20:18:28 +0900

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
+    title: `ちょまど 初 Gatsby on Azure Static Web App`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
     author: `@gatsbyjs`,
   },
